61ced0f4eeb100b91731a693552962d722951f38
ehowlsla <jungler@cvdv.me>
2019-08-22 18:11:09 +0900

    update android backbutton call close event

--- bootpay/src/main/java/kr/co/bootpay/BootpayDialog.java
+++ bootpay/src/main/java/kr/co/bootpay/BootpayDialog.java
@@ -1,7 +1,10 @@
 package kr.co.bootpay;
 
+import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +18,16 @@ public class BootpayDialog extends DialogFragment {
     private Request request;
     protected BootpayWebView bootpay;
     private EventListener listener;
+
+//    @Override
+//    public Dialog onCreateDialog(Bundle savedInstanceState) {
+//        return new Dialog(getActivity(), getTheme()){
+//            @Override
+//            public void onBackPressed() {
+//                //do your stuff
+//            }
+//        };
+//    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -31,16 +44,38 @@ public class BootpayDialog extends DialogFragment {
 
     @Override
     public void onDestroyView() {
-
-//        UserInfo.getInstance(getContext()).update();
         super.onDestroyView();
     }
 
     private void afterViewInit() {
-        if (bootpay != null)
+        if (bootpay != null) {
+            Dialog dialog = getDialog();
+            dialog.setOnKeyListener(new DialogInterface.OnKeyListener() {
+                @Override
+                public boolean onKey(DialogInterface dialogInterface, int keyCode, KeyEvent keyEvent) {
+                    if(keyCode == KeyEvent.KEYCODE_BACK) {
+                        if(listener != null)
+                            listener.onClose("android backbutton clicked");
+                    }
+                    return false;
+                }
+            });
             bootpay.setRequest(request)
-                    .setDialog(getDialog())
+                    .setDialog(dialog)
                     .setOnResponseListener(listener);
+        }
+
+
+
+//        setOnKeyListener(new DialogInterface.OnKeyListener() {
+//            @Override
+//            public boolean onKey(android.content.DialogInterface dialog, int keyCode, android.view.KeyEvent event) {
+//
+//                if ((keyCode == android.view.KeyEvent.KEYCODE_BACK)) {
+//
+//                }
+//            }
+//        }
     }
 
     public BootpayDialog setOnResponseListener(EventListener listener) {
--- sample/src/main/java/bootpay/co/kr/samplepayment/NativeActivity.java
+++ sample/src/main/java/bootpay/co/kr/samplepayment/NativeActivity.java
@@ -32,6 +32,8 @@ public class NativeActivity extends Activity {
 
 
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -76,8 +78,8 @@ public class NativeActivity extends Activity {
         Bootpay.init(getFragmentManager())
                 .setContext(this)
                 .setApplicationId(application_id) // 해당 프로젝트(안드로이드)의 application id 값
-                .setPG(PG.INICIS) // 결제할 PG 사
-                .setMethod(Method.VBANK)
+//                .setPG(PG.KICC) // 결제할 PG 사
+//                .setMethod(Method.CARD)
                 .setBootExtra(bootExtra)
                 .setBootUser(bootUser)
 //                .setUserPhone("010-1234-5678") // 구매자 전화번호
