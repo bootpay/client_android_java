673adaf54ffadeb47ef039de717528414550e80b
ehowlsla <rupy1014@gmail.com>
2020-10-08 15:26:21 +0900

    bootpay 3.3.1 update

--- bootpay/src/main/java/kr/co/bootpay/BootpayWebView.java
+++ bootpay/src/main/java/kr/co/bootpay/BootpayWebView.java
@@ -36,7 +36,7 @@ import kr.co.bootpay.model.Request;
 import kr.co.bootpay.pref.UserInfo;
 
 public class BootpayWebView extends WebView {
-private static final String BOOTPAY = "https://inapp.bootpay.co.kr/3.3.0/production.html";
+private static final String BOOTPAY = "https://inapp.bootpay.co.kr/3.3.1/production.html";
 
     private Dialog dialog;
 //    private ConnectivityManager connManager;
